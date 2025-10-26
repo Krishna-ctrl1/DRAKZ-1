cf26940c14044dcebe820e08144f3e0f9ddeceeb
deepthi-mullapudi <deepthimullapudi@gmail.com>
2025-10-26 13:17:00 +0530

    fix(nav): implement sidebar navigation and fix broken routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/ziko/Home';
-import LoginPage from './components/ziko/LoginPage';
-import Dashboard from './components/deepthi/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/ziko/Home";
+import LoginPage from "./components/ziko/LoginPage";
 import FinBot from "./components/gupta/Finbot";
-import './App.css';
+import Dashboard from "./components/deepthi/Dashboard";
+import InvestmentsPage from "./components/ragamaie/InvestmentsPage"; // <-- ADD THIS
+import BlogPage from "./components/ragamaie/BlogPage";
+import "./App.css";
 
 function App() {
   return (
@@ -15,11 +17,16 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/finbot" element={<FinBot />} />
-          {/* <Route path="/investments" element={<Investments />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/finbot" element={<FinBot />} />
-            <Route path="/privileges" element={<Privileges />} />
-            <Route path="/settings" element={<Settings />} /> */}
+
+          {/* THIS IS THE FIX:
+            The stray {" "} text has been removed from the end of this line.
+          */}
+          <Route path="/investments" element={<InvestmentsPage />} />
+
+          <Route path="/blog" element={<BlogPage />} />
+
+          {/* This catch-all route sends any unknown URL to the dashboard */}
+          <Route path="*" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
