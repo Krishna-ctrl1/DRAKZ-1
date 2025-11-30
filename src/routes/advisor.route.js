8cd5a39a0ad5c1a2e2e7477cea8b2fe143910868
Krishna-ctrl1 <143648025+Krishna-ctrl1@users.noreply.github.com>
2025-11-30 20:22:10 +0530

    advisor css

--- src/routes/advisor.route.js
+++ src/routes/advisor.route.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { getClients } = require('../controllers/advisor.controller.js');
-// In a real app, you would add middleware here to check if the requester is actually an advisor
-// const { verifyToken, isAdvisor } = require('../middlewares/auth.middleware');
 
 router.get('/clients', getClients);
 
