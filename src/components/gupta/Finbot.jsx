c4206bc96e5fba418983703b91060a2f8430fca5
ziko-ahmed <ziko120204@gmail.com>
2025-12-02 01:10:11 +0530

    finbot

--- src/components/gupta/Finbot.jsx
+++ src/components/gupta/Finbot.jsx
@@ -213,7 +213,7 @@ const FinBot = () => {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/financial-advice", {
+      const response = await fetch("http://127.0.0.1:5002/api/financial-advice", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
