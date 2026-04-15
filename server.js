b65270f82ed3499d234d8912b15574fa9eb26330
ziko-ahmed <ziko120204@gmail.com>
2026-04-15 14:42:54 +0530

    fixed whatever wasn't working in the previous eval

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const privilegeRoutes = require("./src/routes/privilege.route.js");
 const spendingsRoutes = require("./src/routes/spending.routes");
 const advisorRoutes = require("./src/routes/advisor.route.js");
 const userRoutes = require("./src/routes/user.routes");
+const userAdvisorRoutes = require("./src/routes/user.advisor.route.js");
 const investmentsRoutes = require("./src/routes/investments.routes.js");
 const accountSummaryRoutes = require("./src/routes/accountSummary.routes.js");
 const settingsRoutes = require("./src/routes/settings.routes.js");
 const contactRoutes = require("./src/routes/contactRoutes.js");
+const logsRoutes = require("./src/routes/logs.routes.js");
+const kycRoutes = require("./src/routes/kyc.routes.js");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./src/config/swagger.config.js");
 
@@ -74,6 +77,7 @@ io.on("connection", (socket) => {
 // API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api/user", userAdvisorRoutes);
 app.use("/api/privilege", privilegeRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/credit-score", require("./src/routes/creditScore.routes"));
@@ -81,9 +85,11 @@ app.use("/api/spendings", spendingsRoutes);
 app.use("/api/cards", require("./src/routes/card.routes"));
 app.use("/api/advisor", advisorRoutes);
 app.use("/api/contact", contactRoutes);
-app.use("/api", investmentsRoutes);
+app.use("/api/investments", investmentsRoutes);
 app.use("/api/account-summary", accountSummaryRoutes);
 app.use("/api/settings", settingsRoutes);
+app.use("/api/logs", logsRoutes);
+app.use("/api/kyc", kycRoutes);
 
 // Global Error Handler Middleware (MUST be last)
 app.use(errorHandler);
